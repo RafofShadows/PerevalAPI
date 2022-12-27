1c3568a64d528b7a2fe5b9e16b669e63b3acc4b7
RafofShadows <pickers97@yandex.ru>
2022-12-27 16:38:00 +0300

    rollback

--- userdata/models.py
+++ userdata/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(verbose_name='Электронная почта', unique=True)
+    name = models.CharField(verbose_name='Имя', max_length=100)
+    fam = models.CharField(verbose_name='Фамилия', max_length=100)
+    otc = models.CharField(verbose_name='Отчество', max_length=100, blank=True)
+    phone = PhoneNumberField(verbose_name='Телефон', null=False, blank=False, unique=True)
+
+
+class Coords(models.Model):
+    latitude = models.FloatField(verbose_name='Широта', default=0.0)
+    longitude = models.FloatField(verbose_name='Долгота', default=0.0)
+    height = models.IntegerField(verbose_name='Высота', default=0)
+
+
+class Images(models.Model):
+    class Meta:
+        db_table = 'pereval_images'
+
+    date_added = models.DateTimeField(verbose_name='Дата добавления')
+    img = models.BinaryField(verbose_name='Изображение')
+
+
+class Areas(models.Model):
+    class Meta:
+        db_table = 'pereval_areas'
+
+    parent = models.IntegerField(default=0)
+    title = models.CharField(verbose_name='Наименование', max_length=255)
+
+
+class PassData(models.Model):
+    class Meta:
+        db_table = 'pereval_added'
+
+    beauty_title = models.CharField(max_length=100)
+    title = models.CharField('Название', max_length=255)
+    other_titles = models.CharField(verbose_name='Альтернативное название', max_length=255, blank=True)
+    connect = models.TextField(verbose_name='Что соединяет', blank=True)
+    add_time = models.DateTimeField(verbose_name='Дата добавления')
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    coords = models.ForeignKey(Coords, verbose_name='Координаты', on_delete=models.CASCADE)
+    level_winter = models.CharField(verbose_name='Уровень сложности зимой', max_length=100, default='')
+    level_spring = models.CharField(verbose_name='Уровень сложности весной', max_length=100, default='')
+    level_summer = models.CharField(verbose_name='Уровень сложности летом', max_length=100, default='')
+    level_autum = models.CharField(verbose_name='Уровень сложности осенью', max_length=100, default='')
+
+    STATUSES = [
+        ('new', 'Новый'),
+        ('pending', 'В работе'),
+        ('accepted', 'информация принята'),
+        ('rejected', 'информация не принята'),
+    ]
+    status = models.CharField(verbose_name='Статус модерации', max_length=100, choices=STATUSES, default='new')
+    image = models.ManyToManyField(Images, through='PerevalImages')
+
+
+class PerevalImages(models.Model):
+    pereval = models.ForeignKey(PassData, on_delete=models.CASCADE)
+    image = models.ForeignKey(Images, on_delete=models.CASCADE)
+
+
+class Activities(models.Model):
+    class Meta:
+        db_table = 'spr_activities_types'
+
+    title = models.CharField(verbose_name='Способ передвижения', max_length=100)
+
+
+
+
