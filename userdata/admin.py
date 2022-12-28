2e227580485819ab23dcf59b69692d650db35216
RafofShadows <pickers97@yandex.ru>
2022-12-28 19:27:31 +0300

    Закончен метод submitData

--- userdata/admin.py
+++ userdata/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Coords, Area, Image, PassData, Level, PerevalImages
+
+admin.site.register(User)
+admin.site.register(Coords)
+admin.site.register(Area)
+admin.site.register(Image)
+admin.site.register(PassData)
+admin.site.register(Level)
+admin.site.register(PerevalImages)
---
+++ userdata/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from userdata.views import submitData, LevelViewSet, UserViewSet, PassDataViewSet, CoordsViewSet, ImageViewSet, \
+    AreaViewSet
+
+router = routers.DefaultRouter()
+
+router.register(r'users', UserViewSet)
+router.register(r'levels', LevelViewSet)
+router.register(r'perevals', PassDataViewSet)
+router.register(r'coords', CoordsViewSet)
+router.register(r'images', ImageViewSet)
+router.register(r'areas', AreaViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('submitdata/', submitData)
+]
