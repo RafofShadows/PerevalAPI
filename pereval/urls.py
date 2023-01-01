ff679935d083f3de8fde6aaf358634924120b570
RafofShadows <pickers97@yandex.ru>
2023-01-01 16:01:26 +0300

    Документация swagger

--- pereval/urls.py
+++ pereval/urls.py
@@ -15,8 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Pereval API',
+        default_version='v1',
+        description='API documentation',
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('userdata.urls')),
+    path('api/v1/',
+         include([
+             path('', include('userdata.urls')),
+             path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-schema"),
+         ])
+         ),
+
 ]
